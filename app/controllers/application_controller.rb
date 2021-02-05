f84eb5e6328f28dbce3350df81eab2f37a4fff5f
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-02-05 12:45:11 +0900

    rubocopの実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :jp_first_name, :jp_last_name, :jp_kana_first_name, :jp_kana_last_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :jp_first_name, :jp_last_name, :jp_kana_first_name, :jp_kana_last_name,
+                                             :birthday])
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.middle_name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.middle_name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
     jp_first_name { Gimei.first.kanji }
     jp_last_name { Gimei.last.kanji }
     jp_kana_first_name { Gimei.first.katakana }
     jp_kana_last_name { Gimei.last.katakana }
-    birthday {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
-
