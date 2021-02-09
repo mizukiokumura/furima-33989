5b0a55b27486f31163d563a1cf2cc1ae880f64d2
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-02-09 17:25:56 +0900

    商品情報編集機能の修正依頼箇所を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,12 +43,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_show
-    @item = Item.find(params[:id])
-    item = @item.user.id
-    user = current_user.id
-    unless user != item
-      render :edit
-    else
+    unless current_user.id == @item.user.id
       redirect_to root_path
     end
   end
